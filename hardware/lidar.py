a4971d47746aa38c3755d9eac7c74ad393cbc462
Zach Lovin <zrlovin@gmail.com>
2015-10-21 02:24:01 -0500

    Make some absolutely massive improvements to the simulated lidar performance.

--- hardware/lidar.py
+++ hardware/lidar.py
@@ -7,10 +7,11 @@ from utils.data_structures import Point2, Point3
 class Base:
     def __init__(self, controller):
 
-        # set 'sensor' angular range, angular resolution, and blackout areas
+        # set 'sensor' angular range, angular resolution, blackout areas, and nonblocking items
         self.angle_range = math.radians(360)
         self.resolution = math.radians(1)
         self.obscured = [(math.radians(40), math.radians(50)), (math.radians(130), math.radians(140)), (math.radians(220), math.radians(230)), (math.radians(310), math.radians(320))]
+        self.non_blocking = [0, 12]  # refer to grid element id's : set 0 (floor) and 12 (start-box) to be non-blocking elements
 
         # controller provides the simulated map
         self.controller = controller
@@ -33,29 +34,26 @@ class Base:
         # get a slightly randomized initial position.r
         cr = self.wrap(position.r - self.angle_range / 2.0 + random.random() * self.resolution, 0.0, 2.0 * math.pi)
 
-        # preallocate results
+        # preallocate results array
         hits = np.ndarray(shape=(snaps, 3))
 
-        # set 0 (floor) and 12 (start-box) to be non-blocking elements
-        non_blocking = [0, 12]
+        # generate rays array
+        rays = np.ndarray(shape=(snaps, 3))
+        rays[:, 0] = position.y
+        rays[:, 1] = position.x
+        rays[:, 2] = np.linspace(cr, cr + self.angle_range, snaps)
 
-        # iterate over all snaps
-        for snap in range(snaps):
+        # while there are hits, increment the ones that haven't hit something nonblocking yet
+        not_hit = np.in1d(self.map[rays[:, 0].astype(int), rays[:, 1].astype(int)], self.non_blocking)
+        while np.any(not_hit):
+            rays[not_hit, 0] -= 2.0 * np.sin(rays[not_hit, 2])  # since origin is upper-left corner instead of lower-left corner, y has to be flipped
+            rays[not_hit, 1] += 2.0 * np.cos(rays[not_hit, 2])  # but cheat a bit and use 2.0 * delta to make things a bit faster
+            not_hit = np.in1d(self.map[rays[:, 0].astype(int), rays[:, 1].astype(int)], self.non_blocking)
 
-            # update current point
-            cy, cx, cr = position.y, position.x, self.wrap(cr + self.resolution, 0.0, 2.0 * math.pi)
-
-            # dx, dy are constant
-            dy, dx = -2.0 * math.sin(cr), 2.0 * math.cos(cr)
-
-            # until something blocking is hit, increment cx, cy with deltas
-            while self.map[int(cy), int(cx)] in non_blocking:
-                cy, cx = dy + cy, dx + cx
-
-            # save the data
-            hits[snap, 0] = math.sqrt((cx - position.x)**2 + (cy - position.y)**2)  # distance from initial point to hit point
-            hits[snap, 1] = self.wrap(cr - position.r, 0.0, 2.0 * math.pi)  # position.r of measurement relative to front of robot
-            hits[snap, 2] = 1.0  # accuracy percent value
+        # map results to hits array
+        hits[:, 0] = np.sqrt(np.square((rays[:, 1] - position.x)) + np.square((rays[:, 0] - position.y)))
+        hits[:, 1] = (rays[:, 2] - position.r) % (2 * np.pi)
+        hits[:, 2] = 1.0  # all the data is perfect - yay
 
         # remove the obscured areas
         for item in self.obscured:
