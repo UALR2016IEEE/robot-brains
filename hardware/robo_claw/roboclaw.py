c6bfd1dba164eec7bcc907a141c2da2082ea979f
Kori <gillskor@gmail.com>
2016-03-17 22:16:27 -0500

    Supressing pointless overflow error. (twitches slightly)

--- hardware/robo_claw/roboclaw.py
+++ hardware/robo_claw/roboclaw.py
@@ -122,19 +122,7 @@ class RoboClaw:
         self.send_command(raw_data)
 
     def reset_motor_positions(self):
-        raw_data = struct.pack(
-            ">BI",
-            constants.SETM1ENCCOUNT,
-            constants.ENCODER_MIDPOINT
-        )
-        self.send_command(raw_data)
-
-        raw_data = struct.pack(
-            ">BI",
-            constants.SETM2ENCCOUNT,
-            constants.ENCODER_MIDPOINT
-        )
-        self.send_command(raw_data)
+        self.send_command(bytes([constants.RESETENC]))
 
     def get_motor_positions(self):
         # Recive Format:
@@ -149,18 +137,18 @@ class RoboClaw:
         underflow = status & 0b00000001
         overflow =  status & 0b00000100
         direction = status & 0b00000010
-        m1_position = m1_position_unsigned - constants.ENCODER_MIDPOINT
-        if underflow or overflow:
-            raise OverflowError
+        m1_position = direction * -1 * m1_position_unsigned
+        # if underflow or overflow:
+        #     raise OverflowError
 
         raw_data = self.ask(constants.GETM2ENC, 7)
         m2_position_unsigned, status = struct.unpack(struct_formatter, raw_data)
         underflow = status & 0b00000001
         overflow =  status & 0b00000100
         direction = status & 0b00000010
-        m2_position = m2_position_unsigned - constants.ENCODER_MIDPOINT
-        if underflow or overflow:
-            raise OverflowError
+        m2_position = direction * -1 * m2_position_unsigned
+        # if underflow or overflow:
+        #     raise OverflowError
 
         return m1_position, m2_position
 
