38d73b165be2da0cdd5c0879917305471bc6c3a4
Kori <gillskor@gmail.com>
2016-04-02 20:29:09 -0500

    upgraded for scoring

--- backup.py
+++ backup.py
@@ -19,16 +19,8 @@ def main(render, debug):
     if debug:
         for scan in brain.lidar.scanner():
             brain.io.send_data(('lidar-test-points', scan))
-    #brain.move((1, 0), align=False)
-    #brain.move((1, 1), align=False)
-    #brain.move((0, 1), align=False)
-
-    brain.align_center()
-    brain.align_angle()
-    brain.move((1, 0))
-    brain.move_until_proc(150)
-    #import pdb; pdb.set_trace()
-    #brain.rotate_180()
+    brain.align_from(math.pi, unit, flip=-1)
+    brain.align_from(1 * math.pi / 2, unit, flip=-1, axis='y')
 
 class Brain:
     def __init__(self, render=False):
@@ -81,7 +73,7 @@ class Brain:
         self.align_angle()
 
     def get_x_scans(self, x):
-        scanner = self.lidar.scanner()
+        scanner = self.lidar.scanner(offset=270)
         scan_agg = next(scanner)
         for id, scan in zip(range(x-1), scanner):
             scan_agg = np.concatenate((scan, scan_agg), axis=1)
@@ -100,6 +92,36 @@ class Brain:
         action.start()
         while not action.complete:
             print(action.target[None], action.estimate_progress()[None])
+
+    def align_from(self, angle, postion_from, flip=1, axis='x'):
+        scan = self.get_x_scans(3)
+        angle_offset = self.align_angle(scan)
+        pos_offset = flip * (self.align_span(scan, angle - angle_offset) - postion_from)
+        action = self.mob.rotate(angle_offset)
+        self.do_action(action)
+        if axis == 'x':
+            action = self.mob.exec_line(Point3(0, axis))
+        if axis == 'y':
+            action = self.mob.exec_line(Point3(pos_offset))
+        self.do_action(action)
+
+        # left_offset = self.align_span(scan, (3 * math.py / 2) + angle_offset)
+        # rear_offset = self.align_span(scan , (math.pi) + angle_offset)
+
+    def do_action(self, action):
+        action.set_status(status)
+        action.start()
+        while not action.complete:
+            pass
+
+    def align_span(self, scan, angle_offset):
+        return scan[0][self.get_closest_point(scan[1], angle_offset)]
+
+
+    def align_angle(self, scan):
+        right_scan = scan[..., np.logical_and(5 * math.pi / 12 < scan[1], scan[1] < 7 * math.pi / 12)]
+        slope, *tail = np.polyfit(*self.pol2cart(right_scan[0], right_scan[1]), 1)
+        return -math.atan(slope)
 
     def align_angle(self, angle_offset=0):
         scan = self.get_x_scans(2)
