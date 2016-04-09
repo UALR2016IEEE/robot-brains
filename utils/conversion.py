ccae3b74fced599dab9fb18dda0e6a584d5af795
Kori <gillskor@gmail.com>
2016-04-09 04:54:29 -0500

    changed config path

--- utils/conversion.py
+++ utils/conversion.py
@@ -6,7 +6,7 @@ class Conversion:
 
         self.config = {}
 
-        with open('config.json', 'r') as f:
+        with open('/home/pi/2016/robot-brains-old/config.json', 'r') as f:
             self.config = json.load(f)
     
         conversion_factor = self.config["map characteristics"]["map size meters"] * 1000.0 / self.config["map characteristics"]["map size pixels"]
